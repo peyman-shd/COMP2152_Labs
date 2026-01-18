adb9b35f8b97a70258e5c84ee3559dfc0b94275e
peyman-shd <shd.peyman@gmail.com>
2026-01-18 05:01:33 -0500

    add q1, q2, q5

---
+++ Week01/lab1.py
@@ -0,0 +1,15 @@
+# Sample Coding Question 01 Week 01
+# Peyman Shahvand
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3:
+
+# Question 4:
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:", userAge)
