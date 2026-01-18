6c75a78aa95c647fe62cb46bf5e1f21869cf3a35
peyman-shd <shd.peyman@gmail.com>
2026-01-18 05:21:28 -0500

    add q1, q2,q3, q4, q5

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -4,9 +4,22 @@
 # Question 2: Defining an array (list)
 my_array = [1, 4, 7, 9]
 
-# Question 3:
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
 
-# Question 4:
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+# result: 0
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+# The temperature today is: 32.600 degrees Celsius
+
 
 # Question 5:
 userAge = int(input("Enter your age: "))
